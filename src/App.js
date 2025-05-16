4fe564fbe3ad8e8219390e34d706362008ee39b0
Hypermicro-code <tony2mat@gmail.com>
2025-05-16 11:16:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Anlegg from './pages/Anlegg';
 import Meldinger from './pages/Meldinger';
 import NyMelding from './pages/NyMelding';
+import OfflineKo from './pages/OfflineKo';
 
 function App() {
   return (
@@ -12,13 +13,15 @@ function App() {
         <Link to="/">Hjem</Link> |{' '}
         <Link to="/anlegg">Anlegg</Link> |{' '}
         <Link to="/meldinger">Meldinger</Link> |{' '}
-        <Link to="/ny-melding">Ny melding</Link>
+        <Link to="/ny-melding">Ny melding</Link> |{' '}
+        <Link to="/offline-ko">Offline kø</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/anlegg" element={<Anlegg />} />
         <Route path="/meldinger" element={<Meldinger />} />
         <Route path="/ny-melding" element={<NyMelding />} />
+        <Route path="/offline-ko" element={<OfflineKo />} />
       </Routes>
     </Router>
   );
