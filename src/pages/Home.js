9e5bc5cffc7d6cde12f779fb94946ddd7d489754
Hypermicro-code <tony2mat@gmail.com>
2025-05-16 08:56:38 +0200

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Velkommen til EasyFlow</h1>
+      <p>Bruk menyen for å navigere.</p>
+    </div>
+  );
+}
+
+export default Home;
