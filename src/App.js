e6ae8e8980bb7d26059d590c197abe7541547399
Hypermicro-code <tony2mat@gmail.com>
2025-05-21 10:54:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
 import NyttAnlegg from './pages/NyttAnlegg';
 import Anlegg from './pages/Anlegg';
 import AnleggDetalj from './pages/AnleggDetalj';
 import NyMelding from './pages/NyMelding';
 import Meldinger from './pages/Meldinger';
 import OfflineKo from './pages/OfflineKo';
+import Home from './pages/Home';
 import SprakVelger from './components/SprakVelger';
 import './i18n';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <Router>
       <div style={{ padding: '20px' }}>
-        <h1>EasyFlow</h1>
+        <h1>{t('app.tittel')}</h1>
 
-        {/* 🌍 Språkvelger */}
         <SprakVelger />
 
-        {/* 🔗 Navigasjon */}
         <nav style={{ marginBottom: '20px' }}>
-          <Link to="/">Hjem</Link> |{' '}
-          <Link to="/anlegg">Anlegg</Link> |{' '}
-          <Link to="/nytt-anlegg">Nytt anlegg</Link> |{' '}
-          <Link to="/meldinger">Meldinger</Link> |{' '}
-          <Link to="/ny-melding">Ny melding</Link> |{' '}
-          <Link to="/offline-ko">Offline-kø</Link>
+          <Link to="/">{t('app.hjem')}</Link> |{' '}
+          <Link to="/anlegg">{t('app.anlegg')}</Link> |{' '}
+          <Link to="/nytt-anlegg">{t('app.nyttAnlegg')}</Link> |{' '}
+          <Link to="/meldinger">{t('app.meldinger')}</Link> |{' '}
+          <Link to="/ny-melding">{t('app.nyMelding')}</Link> |{' '}
+          <Link to="/offline-ko">{t('app.offlineKo')}</Link>
         </nav>
 
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/anlegg" element={<Anlegg />} />
           <Route path="/nytt-anlegg" element={<NyttAnlegg />} />
           <Route path="/anlegg/:id" element={<AnleggDetalj />} />
           <Route path="/meldinger" element={<Meldinger />} />
           <Route path="/ny-melding" element={<NyMelding />} />
           <Route path="/offline-ko" element={<OfflineKo />} />
-          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </Router>
