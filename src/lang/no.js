0a354827461ccd146cf286d80dda854fc3ed30b9
Hypermicro-code <tony2mat@gmail.com>
2025-05-21 07:50:57 +0200

    Create no.js

---
+++ src/lang/no.js
@@ -0,0 +1,10 @@
+export default {
+  nyMelding: {
+    tittel: 'Ny melding',
+    anleggsnummer: 'Anleggsnummer',
+    melding: 'Melding',
+    send: 'Send melding',
+    sendt: 'Melding sendt',
+    lokal: 'Ingen dekning – melding lagret lokalt',
+  }
+};
