9f90d8e5ce7063e7c62e53ebadfebd89dee88e5b
Hypermicro-code <tony2mat@gmail.com>
2025-05-21 13:46:49 +0200

    Update AnleggDetalj.js

--- src/pages/AnleggDetalj.js
+++ src/pages/AnleggDetalj.js
@@ -102,14 +102,13 @@ function AnleggDetalj() {
         <h1>{t('anleggDetalj.tittel')} {statusEmoji(status, erArkivert)}</h1>
 
         <div><strong>{t('anleggDetalj.anleggsnummer')}:</strong> {anlegg.anleggsnummer}</div>
-           {!anlegg.anleggsnummer.toString().includes('-') && (
-  <UnderAnleggBobler hovednummer={anlegg.anleggsnummer} gjeldendeId={anlegg.id} />
+
 )} 
         <div><strong>{t('anleggDetalj.navn')}:</strong> {anlegg.navn}</div>
         <div><strong>{t('anleggDetalj.opprettet')}:</strong> {new Date(anlegg.opprettet).toLocaleString()}</div>
 
-        {!anlegg.anleggsnummer.toString().includes('-') && (
-          <UnderAnleggListe hovednummer={anlegg.anleggsnummer} gjeldendeId={anlegg.id} />
+           {!anlegg.anleggsnummer.toString().includes('-') && (
+  <UnderAnleggBobler hovednummer={anlegg.anleggsnummer} gjeldendeId={anlegg.id} />
         )}
 
         <br />
