eb4faab1598562bbbc4978c4f9c36710c66f261a
Hypermicro-code <tony2mat@gmail.com>
2025-05-21 07:50:25 +0200

    Create i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import no from './lang/no';
+import en from './lang/en';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      no: { translation: no },
+      en: { translation: en },
+    },
+    lng: 'no', // Standard språk
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
