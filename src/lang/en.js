f7662ed82d04e961001dc248aacd4803275a63d9
Hypermicro-code <tony2mat@gmail.com>
2025-05-21 07:51:25 +0200

    Create en.js

---
+++ src/lang/en.js
@@ -0,0 +1,10 @@
+export default {
+  nyMelding: {
+    tittel: 'New message',
+    anleggsnummer: 'Site number',
+    melding: 'Message',
+    send: 'Send message',
+    sendt: 'Message sent',
+    lokal: 'Offline – message saved locally',
+  }
+};
