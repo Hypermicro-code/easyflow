462301e322f4360e51ece926da04b1d2d4c83d2c
Hypermicro-code <tony2mat@gmail.com>
2025-05-22 11:51:34 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,9 +6,9 @@ import { getAuth } from 'firebase/auth';
 
 // Lim inn din egen Firebase-konfigurasjon her:
 const firebaseConfig = {
-  apiKey: "AIzaSyDhpNIsin6Bkvsl7DBhishGxz20cC4DPL4",
-  authDomain: "easyflow-95b3e.firebaseapp.com",
-  projectId: "easyflow-95b3e",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_API_KEY,
+  projectId: process.env.REACT_APP_FIREBASE_API_KEY,
   storageBucket: "easyflow-95b3e.firebasestorage.app",
   messagingSenderId: "821871316593",
   appId: "1:821871316593:web:caaead49048be3b00f95d0"
