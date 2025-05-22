3d19f771d15c3d2bcdd2efa3a5650f0e184021aa
Hypermicro-code <tony2mat@gmail.com>
2025-05-22 09:15:44 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
 
 // Lim inn din egen Firebase-konfigurasjon her:
 const firebaseConfig = {
@@ -16,5 +17,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
-
+// 📦 Eksporter tjenestene du bruker
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
 export { db, storage };
